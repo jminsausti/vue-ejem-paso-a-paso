ff04efac5f30bf87253b0e604e76b90091492e2d
jose <jminsausti@fptxurdinaga.com>
2023-11-02 13:40:04 +0100

    setup components

---
+++ componentes-options-composition/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1>KAIXO WORLD</h1>
+</template>
+
+<style scoped>
+h1{
+  color: red;
+}
+</style>
