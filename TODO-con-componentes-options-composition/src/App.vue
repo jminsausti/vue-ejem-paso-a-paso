f395b2789d09571510db51af7fb44447c6df66a4
jose <jminsausti@fptxurdinaga.com>
2023-11-13 13:18:19 +0100

    setup 3-renderizado listas y 5-eventos

---
+++ TODO-con-componentes-options-composition/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>Diferentes Componentes</h1>
+  
+  <!-- <CounterOption /> -->
+  <!-- <CounterComposition></CounterComposition> -->
+  <!-- <ListasOption></ListasOption> -->
+  <!-- <ListasComposition></ListasComposition> -->
+  <!-- <EventosOption></EventosOption> -->
+  <EventosComposition></EventosComposition>
+</template>
+
+
+<script setup>
+  //import PostContainer from './components/PostContainer.vue';
+  // import CounterOption from './components/CounterOption.vue';
+  // import CounterComposition from './components/CounterComposition.vue';
+//3-Renderizado de Listas
+  // import ListasOption from './components/3-renderizado-listas/3-ListasOption.vue'
+ // import ListasComposition from './components/3-renderizado-listas/ListasComposition.vue';
+//5-Eventos
+  // import EventosOption from './components/4-Eventos/EventosOption.vue'
+  import EventosComposition from './components/4-Eventos/EventosComposition.vue';
+
+</script>
+
+
+<style scoped>
+h1{
+  color: red;
+}
+</style>
