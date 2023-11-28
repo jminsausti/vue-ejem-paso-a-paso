379adabb212044c3af9ada7e138dc5ddeb9ef31e
jose <jminsausti@fptxurdinaga.com>
2023-11-28 12:08:18 +0100

    cambios en acceso a propiedades en 14-Componentes basicos

--- Ejemplos-con-CDN/14-Componentes-props/js/main.js
+++ Ejemplos-con-CDN/14-Componentes-props/js/main.js
@@ -12,19 +12,21 @@ app.component('autor', {
     props: ['nombre', 'edad'],
     mounted() {
         // Las props son accesibles desde this (proxy)
-        //console.log(this.nombre);
+        // console.log(this.nombre);
         console.log(typeof this.edad);
+        this.nombre2=this.nombre //Cogemos el valor de la propiedad y lo asignamos a un avariable local para después modificar su contenido en el componente
     },
     data(){
         return{
-            nombre2:'jeje'
+            nombre2:''
         }
     },
     template: //html
-    `<div><h1> {{ nombre }} --- {{nombre}}</h1><button @click="cambiarProp">Cambiar Prop</button></div>`,
+    `<div><h1> {{ nombre }} --- {{nombre2}}</h1><button @click="cambiarProp">Cambiar Prop</button></div>`,
     methods: {
         cambiarProp() {
-            this.nombre = this.nombre.toUpperCase();
+            //this.nombre = this.nombre.toUpperCase(); No se puede modificar el valor de una propiedad
+             this.nombre2 = this.nombre2.toUpperCase();
         }
     }
 });
