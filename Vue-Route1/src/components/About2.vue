c0244fa4654fca3e2a8f5f61a7f0ea8b26b0bcd0
jose <jminsausti@fptxurdinaga.com>
2024-01-24 09:04:08 +0100

    pinia updated

---
+++ Vue-Route1/src/components/About2.vue
@@ -0,0 +1,15 @@
+
+
+<template>
+
+   
+    <h2>Esto es About2 </h2>
+
+</template>
+
+<script >
+    export default{
+        name: 'About2'
+    }
+</script>
+
--- Vue-Route1/src/views/AboutView.vue
+++ Vue-Route1/src/views/AboutView.vue
@@ -2,8 +2,14 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <About2 />
 </template>
-
+<script>
+  import About2 from '../components/About2.vue'
+  export default{
+    components: {About2}
+  }
+</script>
 <style>
 
 </style>
